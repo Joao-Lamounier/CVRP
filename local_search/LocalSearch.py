4ea9a25c193c09f0e3d3aeaaf48ca4f3e23c0cd1
jo4ovictors <joaovictors1003@gmail.com>
2025-01-27 15:53:44 -0300

    Remoção Arquivos - Benchmark

--- local_search/LocalSearch.py
+++ local_search/LocalSearch.py
@@ -4,6 +4,5 @@ class LocalSearch:
         total_distance = 0.0
         for i in range(len(route) - 1):
             total_distance += distance_matrix[route[i]][route[i + 1]]
-        # Fechando o ciclo ao voltar ao início
         total_distance += distance_matrix[route[-1]][route[0]]
         return total_distance
